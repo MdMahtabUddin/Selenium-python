3ecbd6c6d3363d4a73755ad44ffbcda9909821e8
MdMahtabUddin <mahtab.sentu@outlook.com>
2024-01-16 23:04:06 +0600

    edge browser

---
+++ test2.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+import time
+
+options = webdriver.EdgeOptions()
+options.add_experimental_option("detach", True)
+
+
+driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+
+driver.get('https://www.google.com/')
+
+time.sleep(30)
+driver.quit()
