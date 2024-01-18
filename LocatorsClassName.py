214a7f652035b3f7946f8319d66113f6b22363ee
MdMahtabUddin <mahtab.sentu@outlook.com>
2024-01-18 22:39:25 +0600

    Locators ClassName

---
+++ LocatorsClassName.py
@@ -0,0 +1,29 @@
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+
+driver = webdriver.Chrome(options=options,service=ChromeService(ChromeDriverManager().install()))
+
+driver.get('https://app.hubspot.com/login')
+# driver.find_element(By.CSS_SELECTOR,"#username").send_keys('mahtab45')
+
+# Using Class Name 
+
+# driver.find_element(By.CLASS_NAME,"login-email").send_keys('mahtab.sentu')
+# driver.find_element(By.CLASS_NAME,"login-password").send_keys('bd45')
+
+# Using CSS Selector
+
+# driver.find_element(By.CSS_SELECTOR,"input.form-control.private-form__control.login-email").send_keys('mahtab.sentu')
+# driver.find_element(By.CSS_SELECTOR,"form-control private-form__control login-password m-bottom-3").send_keys('bd45')
+
+# Using Xpath 
+
+driver.find_element(By.XPATH,"//input[@class='form-control private-form__control login-email']").send_keys('mahtab')
+driver.find_element(By.XPATH,"//input[@class='form-control private-form__control login-password m-bottom-3']").send_keys('bd45')
