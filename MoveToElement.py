2bc8014f2c24bcdc629bf9da51f29e2fc7f5978d
MdMahtabUddin <mahtab.sentu@outlook.com>
2024-01-21 19:10:51 +0600

    Move element

---
+++ MoveToElement.py
@@ -0,0 +1,15 @@
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from selenium.webdriver.support.ui import Select
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+
+driver = webdriver.Chrome(options=options,service=ChromeService(ChromeDriverManager().install()))
+
+driver.get('https://app.hubspot.com/login')
+
