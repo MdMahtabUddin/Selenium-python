78e619ddeadf6772553bb59e7ae83779effd6b8f
MdMahtabUddin <mahtab.sentu@outlook.com>
2024-01-27 04:45:51 +0600

    added

--- DragnDrop.py
+++ DragnDrop.py
@@ -13,18 +13,13 @@ options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=options,service=ChromeService(ChromeDriverManager().install()))
 
-driver.get('https://www.spicejet.com/')
+driver.get('https://jqueryui.com/droppable/')
+driver.implicitly_wait(16)
 
-# Move element
+source =driver.find_element(By.ID,'draggable')
+target =driver.find_element(By.ID,'droppable')
 
-login_ele=driver.find_element(By.CSS_SELECTOR,"div.css-76zvg2.r-jwli3a.r-ubezar.r-16dba41.r-1pzd9i8")
-act_chain =ActionChains(driver)
-act_chain.move_to_element(login_ele).perform()
-
-travel_agent= driver.find_element(By.LINK_TEXT,"Visa Services")
-act_chain.move_to_element(travel_agent).perform()
-
-
-
+act_chain=ActionChains(driver)
+act_chain.drag_and_drop(source,target).perform()
 
 
